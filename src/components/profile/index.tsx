89da1f79710031eae4dea1aab28d2f31e05c10fd
Ariel Mulet <mulet.ariel@gmail.com>
2024-01-28 23:45:01 -0300

    simple layout and auth

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+
+'use client';
+import React from 'react';
+import { useUser   } from '@auth0/nextjs-auth0/client';
+
+import Button from '@mui/material/Button'
+
+import './styles.scss'
+
+const Profile = () => {
+   const { user,  error, isLoading } = useUser()
+    
+   if (isLoading) return <div>Loading...</div>;
+   if (error) return <div>{error.message}</div>;
+  
+  
+  return (    
+      <div className='profile'>
+        {user ? (<>
+              <img className='photo' src={user.picture} alt={user.name} />
+              <h2>{user.name}</h2>
+              <p>{user.email}</p></>
+              ) :
+            <Button  variant='contained' href='/api/auth/login' >Login</Button>  
+      }
+      </div>        
+  );
+}
+
+export default Profile
